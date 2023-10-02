ad6e51249375f283e46387b3e114e0349422175f
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-10-03 06:51:39 +0700

    update src/Client.js

--- src/Client.js
+++ src/Client.js
@@ -199,7 +199,8 @@ PROGRESS_MESSAGE: "//*[@id='app']/div/div/div[3]",
 }
 );
 
-const INTRO_IMG_SELECTOR = "[data-icon='search']";
+const INTRO_IMG_SELECTOR = '[data-testid="intro-md-beta-logo-dark"], [data-testid="intro-md-beta-logo-light"], [data-asset-intro-image-light="true"], [data-asset-intro-image-dark="true"],' +
+            '[data-icon="intro-md-beta-logo-dark"], [data-icon="intro-md-beta-logo-light"]';
 const INTRO_QRCODE_SELECTOR = "div[data-ref] canvas";
 
 // Checks which selector appears first
