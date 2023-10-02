dd65aff953bedb09e5ae0844460f643823689ec1
rdp <rdp@my.id>
2023-10-02 05:51:58 -0700

    update all

--- src/structures/Location.js
+++ src/structures/Location.js
@@ -1,12 +1,3 @@
-/*
- * MywaJS 2023
- * re-developed wwebjs
- * using with playwright & wajs
- * contact:
- * wa: 085157489446
- * ig: amirul.dev
- */
-
 'use strict';
 
 /**
@@ -39,4 +30,4 @@ class Location {
     }
 }
 
-export default Location
+module.exports = Location;
