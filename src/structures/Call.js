4014e51d3c67672861bd4c451b71a6e9458cdf3f
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-10-03 10:42:52 +0700

    update src/structures/Call.js

--- src/structures/Call.js
+++ src/structures/Call.js
@@ -69,7 +69,7 @@ class Call extends Base {
     async reject() {
         return this.client.mPage.evaluate(({peerJid, id}) => {
             return window.WWebJS.rejectCall(peerJid, id);
-        }, { peerJid: this.from, this.id });
+        }, { peerJid: this.from, id });
     }
 }
 
