a813c6be5ac509b33e79104c70ce3681bd1a5741
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-10-04 10:36:54 -0700

    Update Constants.js

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -15,7 +15,7 @@ exports.DefaultOptions = {
     qrMaxRetries: 0,
     takeoverOnConflict: false,
     takeoverTimeoutMs: 0,
-    userAgent: 'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
     ffmpegPath: 'ffmpeg',
     bypassCSP: false,
     proxyAuthentication: undefined
