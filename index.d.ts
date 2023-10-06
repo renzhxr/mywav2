e68ccab5039425f889ccd6693c2e37dce2eff1a4
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-10-06 04:15:33 -0700

    update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 
 import { EventEmitter } from 'events'
 import { RequestInit } from 'node-fetch'
-import * as chromium from 'mychromium'
+import * as chromium from '@amiruldev/chromium'
 
 declare namespace WAWebJS {
 
