a0290c1434913372a235d492ed2a16763195e303
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-10-06 16:03:46 -0700

    update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 
 import { EventEmitter } from 'events'
 import { RequestInit } from 'node-fetch'
-import * as chromium from 'puppeteer-extra'
+import * as chromium from 'puppeteer-core'
 
 declare namespace WAWebJS {
 
